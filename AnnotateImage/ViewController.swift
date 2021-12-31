af7a38f29d5ffd9e13737c61cf1edcaa23042e9b
Shyam Shankar <shyam.shankar.s@gmail.com>
2021-12-30 21:10:54 -0800

    Initial Commit

---
+++ AnnotateImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnnotateImage
+//
+//  Created by Shyam Shankar on 12/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AnnotateImageUITests/AnnotateImageUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AnnotateImageUITestsLaunchTests.swift
+//  AnnotateImageUITests
+//
+//  Created by Shyam Shankar on 12/30/21.
+//
+
+import XCTest
+
+class AnnotateImageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
